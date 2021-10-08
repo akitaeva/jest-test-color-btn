09ae4d7c600ab9fe9dd07e8325499c7e9d5a6dd5
Alina Kitaeva <akitaeva@gmail.com>
2021-10-07 20:53:26 -0400

    button test - color and text

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [ btnColor, setBtnColor ] = useState('red');
+  const [ btnText, setBtnText ] = useState('Change to blue');
+  const newBtnColor = btnColor === 'red' ? 'blue' : 'red';
+  const newBtnText = btnText === 'Change to blue' ? 'Change to red' : 'Change to blue'
+
+  const changeTextAndColor = () => {
+    setBtnColor(newBtnColor);
+    setBtnText(newBtnText);
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button style={{backgroundColor: btnColor }} onClick={()=>changeTextAndColor()}>
+        {btnText}
+      </button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('button has the correct initial color', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // find an element with a role of button and correct text
+  const btn = screen.getByRole('button', {name: 'Change to blue'});
+  expect(btn).toHaveStyle({ backgroundColor: 'red'});
+  // click button
+  fireEvent.click(btn);
+  // expect the button's bg color to be blue 
+  expect(btn).toHaveStyle({ backgroundColor: 'blue'});
+  // expect the button text to be 'Change to red'
+  expect(btn.textContent).toBe('Change to red');
 });
+
+
+// test('button turns blue on click', () => {
+//   render(<App />);
+//   const btn = screen.getByRole('button', {name: 'Change to blue'});
+// });
